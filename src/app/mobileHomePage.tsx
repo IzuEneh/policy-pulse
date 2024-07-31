dddc76290593c1dd55cc3b1d8aedd5f8879cbba1
Izu Eneh <izueneh21@gmail.com>
2024-07-30 23:18:51 -0400

    Update home page to work on mobile

--- src/app/mobileHomePage.tsx
+++ src/app/mobileHomePage.tsx
@@ -1,41 +1,49 @@
 import Summary from "@/components/ui/summary";
-import { Card, CardContent } from "@/components/ui/card"
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
 
 interface Props {
-  summaries: any[];
+  sections: {
+    title: string;
+    subtitle: string;
+    data: {
+      id: string;
+      title: string;
+      summary: string;
+    }[];
+  }[];
 }
 
-export default function MobileHome({ summaries }: Props) {
+export default function MobileHome({ sections }: Props) {
   return (
-    <Carousel
-      opts={{
-        align: "start",
-      }}
-      orientation="vertical"
-      className="w-full max-w-xs"
-    >
-      <CarouselContent className="-mt-1 h-[200px]">
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index} className="pt-1 md:basis-1/2">
-            <div className="p-1">
-              <Card>
-                <CardContent className="flex items-center justify-center p-6">
-                  <span className="text-3xl font-semibold">{index + 1}</span>
-                </CardContent>
-              </Card>
+    <main className="flex min-h-screen flex-col items-center gap-2">
+      <h1 className="text-4xl font-bold px-5 pt-5">Recent Policies in your Area</h1>
+      <div className="flex flex-1 flex-col w-full gap-6">
+        {sections.map((section, sectionIndex) => (
+          <>
+            <div className="flex flex-1 flex-col p-5" key={sectionIndex}>
+              <h2 className="text-2xl capitalize font-bold">{section.title}</h2>
+              <p className="text-lg">{section.subtitle}</p>
+              <div className="flex flex-1 flex-col gap-4 mt-3">
+                {section.data.map((item, index) => (
+                  <>
+                    <Summary
+                      key={item.id}
+                      id={item.id}
+                      title={item.title}
+                      summary={item.summary}
+                    />
+                    {index !== section.data.length - 1 ? (
+                      <div className="w-full border bg-slate-700"></div>
+                    ) : null}
+                  </>
+                ))}
+              </div>
             </div>
-          </CarouselItem>
+            {sectionIndex !== sections.length - 1 ? (
+              <div className="w-100 h-3 border bg-slate-400"></div>
+            ) : null}
+          </>
         ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+      </div>
+    </main>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,110 +1,106 @@
-'use client'
+"use client";
 
 import Summary from "@/components/ui/summary";
-import { useMediaQuery } from '@/hooks/useMediaQuery'
+import { useMediaQuery } from "@/hooks/useMediaQuery";
 
-import MobileHome from "@/app/mobileHomePage"
+import MobileHome from "@/app/mobileHomePage";
 
 const summaries = [
-	{
-		id: "bill-0",
-		title:
-			"Servicemember Quality of Life Improvement and National Defense Authorization Act for Fiscal Year 2025",
-		summary: `This act authorizes appropriations for fiscal year 2025 for military activities of the Department of Defense,
-    military construction, and defense activities of the Department of Energy. It also prescribes military personnel strengths
-    and addresses various aspects of military operations and personnel matters.`,
-	},
-	{
-		id: "bill-1",
-		title: "Preventing Sex Trafficking and Strengthening Families Act",
-		summary: `The "Preventing Sex Trafficking and Strengthening Families Act" aims to prevent and address sex trafficking of children in foster care,
-      extend and improve adoption incentives, and enhance international child support recovery.`,
-	},
   {
-		id: "bill-0",
-		title:
-			"Servicemember Quality of Life Improvement and National Defense Authorization Act for Fiscal Year 2025",
-		summary: `This act authorizes appropriations for fiscal year 2025 for military activities of the Department of Defense,
+    id: "bill-0",
+    title:
+      "Servicemember Quality of Life Improvement and National Defense Authorization Act for Fiscal Year 2025",
+    summary: `This act authorizes appropriations for fiscal year 2025 for military activities of the Department of Defense,
     military construction, and defense activities of the Department of Energy. It also prescribes military personnel strengths
     and addresses various aspects of military operations and personnel matters.`,
-	},
-	{
-		id: "bill-1",
-		title: "Preventing Sex Trafficking and Strengthening Families Act",
-		summary: `The "Preventing Sex Trafficking and Strengthening Families Act" aims to prevent and address sex trafficking of children in foster care,
-      extend and improve adoption incentives, and enhance international child support recovery.`,
-	},
+  },
   {
-		id: "bill-0",
-		title:
-			"Servicemember Quality of Life Improvement and National Defense Authorization Act for Fiscal Year 2025",
-		summary: `This act authorizes appropriations for fiscal year 2025 for military activities of the Department of Defense,
+    id: "bill-1",
+    title: "Preventing Sex Trafficking and Strengthening Families Act",
+    summary: `The "Preventing Sex Trafficking and Strengthening Families Act" aims to prevent and address sex trafficking of children in foster care,
+      extend and improve adoption incentives, and enhance international child support recovery.`,
+  },
+  {
+    id: "bill-0",
+    title:
+      "Servicemember Quality of Life Improvement and National Defense Authorization Act for Fiscal Year 2025",
+    summary: `This act authorizes appropriations for fiscal year 2025 for military activities of the Department of Defense,
     military construction, and defense activities of the Department of Energy. It also prescribes military personnel strengths
     and addresses various aspects of military operations and personnel matters.`,
-	},
-	{
-		id: "bill-1",
-		title: "Preventing Sex Trafficking and Strengthening Families Act",
-		summary: `The "Preventing Sex Trafficking and Strengthening Families Act" aims to prevent and address sex trafficking of children in foster care,
+  },
+  {
+    id: "bill-1",
+    title: "Preventing Sex Trafficking and Strengthening Families Act",
+    summary: `The "Preventing Sex Trafficking and Strengthening Families Act" aims to prevent and address sex trafficking of children in foster care,
       extend and improve adoption incentives, and enhance international child support recovery.`,
-	},
+  },
+  {
+    id: "bill-0",
+    title:
+      "Servicemember Quality of Life Improvement and National Defense Authorization Act for Fiscal Year 2025",
+    summary: `This act authorizes appropriations for fiscal year 2025 for military activities of the Department of Defense,
+    military construction, and defense activities of the Department of Energy. It also prescribes military personnel strengths
+    and addresses various aspects of military operations and personnel matters.`,
+  },
+  {
+    id: "bill-1",
+    title: "Preventing Sex Trafficking and Strengthening Families Act",
+    summary: `The "Preventing Sex Trafficking and Strengthening Families Act" aims to prevent and address sex trafficking of children in foster care,
+      extend and improve adoption incentives, and enhance international child support recovery.`,
+  },
+];
+
+const sections = [
+  {
+    title: "Fedral Laws",
+    subtitle: "Current laws being discussed at the federal level",
+    data: summaries,
+  },
+  {
+    title: "State Laws",
+    subtitle: "Current laws being discussed at the state level",
+    data: summaries,
+  },
 ];
 
 export default function Home() {
-	const isMobile = useMediaQuery('(max-width: 600px)')
+  const isMobile = useMediaQuery("(max-width: 600px)");
 
-	if (isMobile) {
-		return <MobileHome summaries={summaries} />
-	}
+  if (isMobile) {
+    return <MobileHome sections={sections} />;
+  }
 
-	return (
-		<main className="flex min-h-screen flex-col items-center gap-5 p-5">
-			<h1 className="text-4xl">Recent Policies in your Area</h1>
-			<div className="flex flex-1 flex-row w-full">
-				<div className="flex flex-1 flex-col p-6">
-					<h2 className="text-2xl capitalize font-bold">Fedral Laws</h2>
-					<p className="text-lg">
-						Current laws being discussed at the federal level
-					</p>
-					<div className="flex flex-1 flex-col gap-4 mt-3">
-						{summaries.map((item, index) => (
-							<>
-								<Summary
-									key={item.id}
-									id={item.id}
-									title={item.title}
-									summary={item.summary}
-								/>
-								{index !== summaries.length - 1 ? (
-									<div className="w-full border bg-slate-700"></div>
-								) : null}
-							</>
-						))}
-					</div>
-				</div>
-				<div className="h-100 border bg-slate-700"></div>
-				<div className="flex flex-1 flex-col p-6">
-					<h2 className="text-2xl capitalize font-bold">State Laws</h2>
-					<p className="text-lg">
-						Current laws being discussed at the state level
-					</p>
-					<div className="flex flex-1 flex-col gap-4 mt-3">
-						{summaries.map((item, index) => (
-							<>
-								<Summary
-									key={item.id}
-									id={item.id}
-									title={item.title}
-									summary={item.summary}
-								/>
-								{index !== summaries.length - 1 ? (
-									<div className="w-full border bg-slate-700"></div>
-								) : null}
-							</>
-						))}
-					</div>
-				</div>
-			</div>
-		</main>
-	);
+  return (
+    <main className="flex min-h-screen flex-col items-center gap-5 p-5">
+      <h1 className="text-4xl">Recent Policies in your Area</h1>
+      <div className="flex flex-1 flex-row w-full">
+        {sections.map((section, sectionIndex) => (
+          <>
+            <div className="flex flex-1 flex-col p-6">
+              <h2 className="text-2xl capitalize font-bold">{section.title}</h2>
+              <p className="text-lg">{section.subtitle}</p>
+              <div className="flex flex-1 flex-col gap-4 mt-3">
+                {section.data.map((item, index) => (
+                  <>
+                    <Summary
+                      key={item.id}
+                      id={item.id}
+                      title={item.title}
+                      summary={item.summary}
+                    />
+                    {index !== section.data.length - 1 ? (
+                      <div className="w-full border bg-slate-700"></div>
+                    ) : null}
+                  </>
+                ))}
+              </div>
+            </div>
+            {sectionIndex !== sections.length - 1 ? (
+              <div className="h-100 border bg-slate-700"></div>
+            ) : null}
+          </>
+        ))}
+      </div>
+    </main>
+  );
 }
