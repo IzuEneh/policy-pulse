8e2d6e0163535e0bf3170e61d198b27a1bf2dda7
Izu Eneh <izueneh21@gmail.com>
2024-08-16 23:32:40 -0400

    remove js ai stuff

--- src/summarizer.ts
+++
@@ -1,6 +0,0 @@
-import "cheerio";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { DocxLoader } from "@langchain/community/document_loaders/fs/docx";
-
-const loader = new DocxLoader("./../policies/bill-1.txt");
-const docs = await loader.load();
