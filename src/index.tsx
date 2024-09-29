616c15fab935db96e754ae3a5871d5efc168968b
Izu Eneh <izueneh21@gmail.com>
2024-09-29 01:25:48 -0400

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -25,46 +25,68 @@ const app = new Elysia()
     const summaries = query.all();
 
     return (
-    <html lang="en">
-      <head>
-        <title>Policy Pulse</title>
-        <script src="https://cdn.tailwindcss.com"></script>
-      </head>
-      <body>
-        <main class="flex min-h-screen flex-col items-center gap-5 p-5">
-          <h1 class="text-4xl">Recent Policies in your Area</h1>
-          <div class="flex flex-1 flex-col w-full">
-            {summaries.map((section, sectionIndex) => (
-              <a href={`/${section.id}`}>
-                <div class="flex flex-1 flex-col p-6">
-                  <h2 class="text-2xl capitalize font-bold">{section.title}</h2>
-                  <p>{section.summary}</p>
-                </div>
-              </a>
-            ))}
-          </div>
-        </main>
-      </body>
-    </html>
-  )})
+      <html lang="en">
+        <head>
+          <title>Policy Pulse</title>
+          <script src="https://cdn.twind.style"></script>
+          <script src="https://unpkg.com/htmx.org@1.9.12"></script>
+        </head>
+        <body>
+          <main class="flex min-h-screen flex-col items-center gap-5 p-5">
+            <h1 class="text-4xl">Recent Policies in your Area</h1>
+            <form
+              class="flex w-full border-2 border-black py-2 px-5 rounded-full "
+              hx-get="/search"
+              hx-target="#results"
+              hx-swap="innerHtml"
+            >
+              <input
+                type="text"
+                name="query"
+                placeholder="Search for a specific document"
+                class="outline-none w-full"
+              />
+              <button type="submit">Enter</button>
+            </form>
+            <div class="flex flex-1 flex-col w-full" id="results">
+              {summaries.map((section, sectionIndex) => (
+                <a href={`/${section.id}`}>
+                  <div class="flex flex-1 flex-col p-6">
+                    <h2 class="text-2xl capitalize font-bold">
+                      {section.title}
+                    </h2>
+                    <p>{section.summary}</p>
+                  </div>
+                </a>
+              ))}
+            </div>
+          </main>
+        </body>
+      </html>
+    );
+  })
   .get("/:id", ({ params: { id } }) => {
     const messages: { role: string; content: string }[] = [];
 
-    const summaryQuery = db.query(`SELECT * FROM summary WHERE id = $id`).as(Summary);
-    const notableQuery = db.query(`SELECT * FROM notable_section WHERE bill_id = $id`).as(NotableSection);
+    const summaryQuery = db
+      .query(`SELECT * FROM summary WHERE id = $id`)
+      .as(Summary);
+    const notableQuery = db
+      .query(`SELECT * FROM notable_section WHERE bill_id = $id`)
+      .as(NotableSection);
 
-    const fullSummary = summaryQuery.get({$id: id});
-    const notableSections = notableQuery.all({ $id: id});
+    const fullSummary = summaryQuery.get({ $id: id });
+    const notableSections = notableQuery.all({ $id: id });
 
     if (!fullSummary) {
-      return redirect('/');
+      return redirect("/");
     }
 
     return (
       <html lang="en">
         <head>
           <title>{fullSummary.title}</title>
-          <script src="https://cdn.tailwindcss.com"></script>
+          <script src="https://cdn.twind.style"></script>
           <script src="https://unpkg.com/htmx.org@1.9.12"></script>
         </head>
         <body>
@@ -88,20 +110,15 @@ const app = new Elysia()
                   ))}
                 </div>
               </div>
-              <div class="flex flex-1 py-2 flex-col bg-slate-200 rounded-lg p-4 gap-3">
-                <div class="flex flex-1 flex-col gap-2" id="messages">
-                  {messages.map((message, index) => (
-                    <div
-                      class={`flex bg-white justify-center items-center p-2 ${
-                        message.role === "user" ? "self-end" : "self-start"
-                      } message`}
-                    >
-                      <p> {message.content}</p>
-                    </div>
-                  ))}
+              <div class="flex flex-1 py-2 flex-col bg-slate-400 rounded-lg p-4 gap-3">
+                <div
+                  class="flex flex-1 flex-col gap-2 items-center justify-center"
+                  id="messages"
+                >
+                  <p>Chat coming soon !</p>
                 </div>
                 <form
-                  class="flex w-full bg-white py-2 px-5 rounded-full"
+                  class="flex w-full bg-slate-200 py-2 px-5 rounded-full "
                   hx-post={`/chat/${id}`}
                   hx-target="#messages"
                   hx-swap="innerHtml"
@@ -111,9 +128,12 @@ const app = new Elysia()
                     type="text"
                     name="message"
                     placeholder="Chat with document..."
-                    class="outline-none w-full"
+                    class="outline-none w-full disabled:bg-slate-200"
+                    disabled
                   />
-                  <button type="submit">Enter</button>
+                  <button type="submit" disabled>
+                    Enter
+                  </button>
                 </form>
               </div>
             </div>
@@ -122,30 +142,38 @@ const app = new Elysia()
       </html>
     );
   })
-  .post("/chat/:id", (context) => {
-    console.log(context.body);
+  .get("/search", ({ query }) => {
+    if (!query["query"]) {
+      return null;
+    }
+
+    const decodedQuery = decodeURI(query["query"]);
+    const searchQuery = db.query(`SELECT id FROM titles($query)`);
+    const searchResults = searchQuery.all({ $query: decodedQuery });
+    console.log("search results: ", searchResults);
+    const summaryQuery = db
+      .query(
+        `SELECT * FROM summary WHERE id IN (${searchResults.map(
+          (res) => `'${res.id}'`
+        )})`
+      )
+      .as(Summary);
+    const results = summaryQuery.all();
+
+    if (!searchResults) {
+      return null;
+    }
+
     return (
       <>
-        <div
-          class={`flex bg-white justify-center items-center p-2 ${"self-start"} message`}
-        >
-          <p> This is a response message</p>
-          <input
-            type="hidden"
-            name="system-1"
-            value="This is a response message"
-          />
-        </div>
-        <div
-          class={`flex bg-white justify-center items-center p-2 ${"self-end"} message`}
-        >
-          <p> This is a user message</p>
-          <input
-            type="hidden"
-            name="user-1"
-            value="This is a user message"
-          />
-        </div>
+        {results.map((result) => (
+          <a href={`/${result.id}`} class="search-result">
+            <div class="flex flex-1 flex-col p-6">
+              <h2 class="text-2xl capitalize font-bold">{result.title}</h2>
+              <p>{result.summary}</p>
+            </div>
+          </a>
+        ))}
       </>
     );
   })
